8d79240a6262b7b9120a7e516259d9447729b7a0
caffine_Overflow <tnzajk@gmail.com>
2024-11-07 16:23:19 +0530

    collecting user input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import "./AddUser.css";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredName, enteredAge);
+
+    setEnteredName("");
+    setEnteredAge("");
+  };
+  const nameChangeHandlter = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
   return (
     <div className="input">
@@ -13,12 +26,22 @@ const AddUser = () => {
         <form onSubmit={addUserHandler}>
           <div className="input">
             <label htmlFor="username">UserName</label>
-            <input type="text" id="username"></input>
+            <input
+              type="text"
+              id="username"
+              value={setEnteredName}
+              onChange={nameChangeHandlter}
+            ></input>
           </div>
 
           <div className="input">
             <label htmlFor="age">Age</label>
-            <input type="number" id="age"></input>
+            <input
+              type="number"
+              id="age"
+              value={setEnteredAge}
+              onChange={ageChangeHandler}
+            ></input>
           </div>
 
           <Button type="submit">Add User</Button>
