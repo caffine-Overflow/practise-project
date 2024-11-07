e62a7b1fe50b5af8d6bb3959bcf5b49575d0e1ac
caffine_Overflow <tnzajk@gmail.com>
2024-11-07 13:37:27 +0530

    Reusable Card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,29 @@
 import React from "react";
+import Card from "../UI/Card";
+import "./AddUser.css";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input type="text" id="username"></input>
+    <div className="input">
+      <Card className="card ">
+        <form onSubmit={addUserHandler}>
+          <div className="input">
+            <label htmlFor="username">UserName</label>
+            <input type="text" id="username"></input>
+          </div>
 
-      <label htmlFor="age">Age</label>
-      <input type="number" id="age"></input>
+          <div className="input">
+            <label htmlFor="age">Age</label>
+            <input type="number" id="age"></input>
+          </div>
 
-      <button type="submit">Add User</button>
-    </form>
+          <button type="submit">Add User</button>
+        </form>
+      </Card>
+    </div>
   );
 };
 export default AddUser;
