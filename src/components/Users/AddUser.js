1fef4c6727dd1839730ba4c15342c7eb2ac2a06f
caffine_Overflow <tnzajk@gmail.com>
2024-11-07 15:31:52 +0530

    Button Componenet

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
 import "./AddUser.css";
+import Button from "../UI/Button";
 
 const AddUser = () => {
   const addUserHandler = (event) => {
@@ -20,7 +21,7 @@ const AddUser = () => {
             <input type="number" id="age"></input>
           </div>
 
-          <button type="submit">Add User</button>
+          <Button type="submit">Add User</Button>
         </form>
       </Card>
     </div>
